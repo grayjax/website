7a97b938917c08366628db1b0cd7d0eaf54f4e5d
vagrant <vagrant@precise32.(none)>
2014-04-14 19:44:58 +0000

    did it

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-var express = require("express");
+var express = require("express")
+, path = require('path');
 
-var app = express()
-var port = 8000;
-app.listen(8000);
-console.log("App listening on port: " + port);
+var app = express();
+
+require('./routes')(app);
+
+
+
+app.use(express.static(path.normalize(__dirname) + '/public'))
 
 app.get('/', function(req, res) {
 	res.send('Hello, welcome to CS1501');
+
+
+var port = Number(process.env.PORT || 8000);
+app.listen(port, function() {
+	console.log("Express app listening on port" + port);
 });
